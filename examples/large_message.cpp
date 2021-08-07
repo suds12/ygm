7b405c0f0ad936a2db9d7c858c66f14e558e1ac9
Sudharshan Srinivasan <ssriniv@catalyst160.llnl.gov>
2021-08-06 17:53:46 -0700

    Refactored listener to handle transit

--- examples/large_message.cpp
+++ examples/large_message.cpp
@@ -5,42 +5,49 @@
 
 #include <ygm/comm.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
   ygm::comm world(&argc, &argv);
 
-  std::string small_message1 = "Ground Control to Major Tom"; //43
-  //std::string small_message2 = "T";
-  std::string small_message3 = "Che ";   //28
-  std::string small_message4 = "jojolokocsdd ";
-  std::string small_message5 = "hahaha ";
-  std::string small_message6 = "kojohgiugkfvsadsddqf ";
+  std::string small_message1 = "Ground Control to Major Tom"; // 43
+  // std::string small_message2 = "T";
+  std::string small_message3 = "small msg "; // 28
+  std::string small_message4 = "bigggggggggggggger message ";
+  std::string small_message5 = "message ";
+  std::string small_message6 = "aab ";
 
-  std::string large_message = "Take your protein pills and put your helmet on. Commencing countdown, engines on. Check ignition and may God's love be with you.";
+  std::string large_message =
+      "Take your protein pills and put your helmet on. Commencing countdown, "
+      "engines on. Check ignition and may God's love be with you.";
 
   // Define the function to execute in async
-  auto howdy = [](auto pcomm, int from, const std::string& str) {
-    std::cout << "Howdy, I'm rank " << pcomm->rank() << " (local rank " 
+  auto howdy = [](auto pcomm, int from, const std::string &str) {
+    std::cout << "Howdy, I'm rank " << pcomm->rank() << " (local rank "
               << pcomm->local_rank() << ", remote rank " << pcomm->remote_rank()
-              << "), and I received a message from rank " << from 
+              << "), and I received a message from rank " << from
               << " that read: \"" << str << "\"" << std::endl;
   };
 
-  //0 send small message to everyone and large message only to 1
+  // 0 send small message to everyone and large message only to 1
   if (world.rank() == 2) {
     // for (int dest = 0; dest < world.size(); ++dest) {
     //   world.async(dest, howdy, small_message);
     // }
 
-     world.async(1, howdy, small_message1);
-     world.async(3, howdy, small_message3);
-     world.async(4, howdy, small_message4);
-     world.async(4, howdy, small_message5);
-     world.async(4, howdy, small_message6);
-     //world.async(5, howdy, small_message6);
-
-
-    // world.async(1, howdy, small_message);
+    world.async(1, howdy, small_message1);
+    world.async(3, howdy, small_message3);
+    world.async(4, howdy, small_message4);
+    world.async(4, howdy, small_message5);
+    world.async(4, howdy, small_message6);
+    world.async(5, howdy, small_message6);
+  }
+  world.barrier();
+  if (world.rank() == 4) {
+    world.async(1, howdy, small_message1);
+    world.async(3, howdy, small_message3);
+    world.async(5, howdy, small_message4);
+    world.async(2, howdy, small_message5);
+    world.async(2, howdy, small_message6);
   }
   return 0;
 }
